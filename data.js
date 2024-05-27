2ed8702a174bc0a4b273b4da8600cdf26f63cf15
jhernandez504 <jeremy.hernandez504@gmail.com>
2024-05-27 17:25:54 -0500

    finished Step 1-7

--- data.js
+++ data.js
@@ -11,30 +11,70 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+var animal = {};
+//push key species value dog to object
+animal.species = 'Dog';
+//push key name with value 'Cookie'
+animal['name'] = 'Cookie';
+//creates noises key inside animal object and assigns empty array
+animal.noises = [];
+//logs animal object to console
+console.log(animal);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//create empty var array named noises
+var noises = [];
+//give noises first element using bracket notation
+noises[0] = 'Woof!';
+//add another element to end of array
+noises.push('Meow!');
+//add element to front of array using array method
+noises.unshift('Grr!');
+//add another element to end
+noises.push('Wooo!');
+
+//log length of the array noises
+console.log(noises.length);
+//log last element of array noises
+console.log(noises[noises.length - 1]);
+//log whole array
+console.log(noises);
+
+
+
+
+
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+//assigning noises arr to prop of animal obj
+animal.noises = noises; 
+//add another element to noises arr inn animal obj key noises
+animal['noises'][animal['noises'].length] = 'Yeet!';
+console.log(animal);
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
- * 2. What are the different ways of accessing elements on arrays?
- *
- * *******************************************************************
  */
+console.log(animal['name']);
+console.log(animal['species']);
+
+/** 
+ * 2. What are the different ways of accessing elements on arrays?
+ */
+console.log(animal['noises'][2]);
+/** 
+ * 
+ * *******************************************************************
+ **/
 
 /* *******************************************************************
  * Step 5 - Take a Break!
@@ -48,14 +88,52 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//declare var animals and assign empty array
+var animals = [];
+//push animal obj to animal array
+animals.push(animal);
+//logs animals array 
+console.log(animals);
+//create duck obj with values
+var duck = { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+//push duck obj to animals array
+animals.push(duck);
+console.log(animals);
 
+//create two more animal obj and push to animals array
+var cat = {
+  species: 'cat',
+  name: 'Kimbap',
+  noises: ['Meow!', 'Hiss']
+}
+animals.push(cat);
 
+var pig = {
+  species: 'pig',
+  name: 'Wilbert',
+  noises: ['Oink!', 'Grunt', 'Skree!']
+};
+animals.push(pig);
 
+//log animals array length
+console.log(animals.length);
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+// 1) I would choose an Array structure to hold friends
+//create array friends
+var friends = [];
+//function getRandom to pull random friend from animal array
+function getRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+console.log(getRandom(animals));
+//adds animal name to friends array from animals[at random index]
+friends.push(getRandom(animals)['name']);
+console.log(friends);
+//adds friends property to animals array using bracket notation
+animals[0].friends = friends;
+console.log(animals);
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
